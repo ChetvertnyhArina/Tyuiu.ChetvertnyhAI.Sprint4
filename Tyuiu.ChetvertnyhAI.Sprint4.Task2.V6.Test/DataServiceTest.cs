ca178e3a8fa3699bb03fa8c92e4e9007f54bd107
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-11-10 00:48:08 +0500

    Task2

---
+++ Tyuiu.ChetvertnyhAI.Sprint4.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint4.Task2.V6.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint4.Task2.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 9, 8, 4, 6, 9, 4, 3, 6, 1, 2 };
+            int res = ds.Calculate(numsArray);
+            int wait = 243;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ChetvertnyhAI.Sprint4.Task2.V6/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint4.Task2.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #4 | Выполнила: Четвертных А. И. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                   *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнила: Четвертных Арина Игоревна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 15 элементов заполненный         *");
+        Console.WriteLine("* случайными значениями в диапазоне от 2 до 8 подсчитать произведение     *");
+        Console.WriteLine("* нечетных элементов массива.                                             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Random rnd = new Random();
+        DataService ds = new DataService();
+
+        
+        Console.Write("Введите количество элементов массива: ");
+        int len = Convert.ToInt32(Console.ReadLine());
+
+        int[] numsArray = new int[len];
+
+        for (int i = 0; i < len; i++)
+        {
+            numsArray[i] = rnd.Next(2, 9);
+        }
+        Console.WriteLine();
+        Console.WriteLine("Массив: ");
+        for (int i = 0; i < len; i++)
+        {
+            Console.Write(numsArray[i] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int res = ds.Calculate(numsArray);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
