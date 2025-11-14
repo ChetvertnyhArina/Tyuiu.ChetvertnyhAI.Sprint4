f0e00910fb9606f1d2527682b270f4aa2cbc59be
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-11-14 22:35:20 +0500

    Task7

---
+++ Tyuiu.ChetvertnyhAI.Sprint4.Task7.V22.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint4.Task7.V22.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint4.Task7.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            int n = 5;
+            int m = 3;
+            int[,] mtrx = new int[n, m];
+            string str = "695324951753684";
+            DataService ds = new DataService();
+            int res = ds.Calculate(n, m, str);
+            int wait = 9216;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ChetvertnyhAI.Sprint4.Task7.V22/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint4.Task7.V22.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #4 | Выполнила: Четвертных А. И. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
+        Console.WriteLine("* Выполнила: Четвертных Арина Игоревна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дана строка из одноразрядных цифр '695324951753684'. Преобразуйте ее    *");
+        Console.WriteLine("* в матрицу 5 на 3 и подсчитайте произведение четных чисел.               *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int n = 5;
+        int m = 3;
+        int[,] mtrx = new int[n, m];
+        string str = "695324951753684";
+        DataService ds = new DataService();
+
+
+        int index = 0;
+        Console.WriteLine("\n Массив:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write($"{str[index]} \t");
+                index++;
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int res = ds.Calculate(n, m, str);
+        Console.WriteLine("Произведение четных чисел = " + res);
+        Console.ReadKey();
+    }
+}
+   
